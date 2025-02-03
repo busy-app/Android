5332c188a17e5226871616db6ecdef3f606dc577
Roman Makeev <makeevrserg@gmail.com>
2025-02-03 15:36:57 +0300

    add stop session bottom sheet

--- components/bsb/timer/active/impl/src/commonMain/kotlin/com/flipperdevices/bsb/timer/active/api/ActiveTimerScreenDecomposeComponentImpl.kt
+++ components/bsb/timer/active/impl/src/commonMain/kotlin/com/flipperdevices/bsb/timer/active/api/ActiveTimerScreenDecomposeComponentImpl.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.childContext
 import com.flipperdevices.bsb.preference.api.ThemeStatusBarIconStyleProvider
 import com.flipperdevices.bsb.timer.active.composable.StopButtonComposable
 import com.flipperdevices.bsb.timer.active.composable.TimerActiveComposableScreen
@@ -32,9 +33,18 @@ import software.amazon.lastmile.kotlin.inject.anvil.ContributesBinding
 class ActiveTimerScreenDecomposeComponentImpl(
     @Assisted componentContext: ComponentContext,
     iconStyleProvider: ThemeStatusBarIconStyleProvider,
-    private val timerApi: TimerApi
+    private val timerApi: TimerApi,
+    private val stopSessionSheetDecomposeComponentFactory: StopSessionSheetDecomposeComponent.Factory
 ) : ActiveTimerScreenDecomposeComponent(componentContext),
     StatusBarIconStyleProvider by iconStyleProvider {
+
+    private val stopSessionSheetDecomposeComponent = stopSessionSheetDecomposeComponentFactory.invoke(
+        childContext("stopSessionSheetDecomposeComponent"),
+        onConfirm = {
+            timerApi.stopTimer()
+        }
+    )
+
     @Composable
     override fun Render(modifier: Modifier) {
         timerApi.getState().collectAsState().value?.let { timerState ->
@@ -42,11 +52,14 @@ class ActiveTimerScreenDecomposeComponentImpl(
                 modifier = modifier,
                 workPhaseText = null,
                 timerState = timerState,
-                onStopClick = {},
+                onStopClick = {
+                    stopSessionSheetDecomposeComponent.show()
+                },
                 onPauseClick = { timerApi.onAction(TimerAction.PAUSE) },
                 onSkipClick = {},
             )
         }
+        stopSessionSheetDecomposeComponent.Render(Modifier)
     }
 
     @Inject
