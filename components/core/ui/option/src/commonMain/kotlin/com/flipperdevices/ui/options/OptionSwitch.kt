50e44c189eb6864fa1064c76a38ce3837e857045
Roman Makeev <makeevrserg@gmail.com>
2025-02-17 15:20:20 +0300

    fix detekt

--- components/core/ui/option/src/commonMain/kotlin/com/flipperdevices/ui/options/OptionSwitch.kt
+++ components/core/ui/option/src/commonMain/kotlin/com/flipperdevices/ui/options/OptionSwitch.kt
@@ -9,8 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material3.Switch
 import androidx.compose.material.Text
+import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
