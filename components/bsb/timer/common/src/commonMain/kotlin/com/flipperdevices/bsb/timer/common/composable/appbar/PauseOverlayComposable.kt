84376bb8b6eb2a45b5d8ec238e2670a85661172a
Nikita Kulikov <n@kulikov.uk>
2025-02-25 19:55:14 +0000

    Fix lint

--- components/bsb/timer/common/src/commonMain/kotlin/com/flipperdevices/bsb/timer/common/composable/appbar/PauseOverlayComposable.kt
+++ components/bsb/timer/common/src/commonMain/kotlin/com/flipperdevices/bsb/timer/common/composable/appbar/PauseOverlayComposable.kt
@@ -1,19 +1,15 @@
 package com.flipperdevices.bsb.timer.common.composable.appbar
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
