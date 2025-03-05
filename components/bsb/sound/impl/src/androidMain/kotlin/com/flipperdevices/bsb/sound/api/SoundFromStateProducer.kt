139aee5e476d642c4aa9d5071fbc6c72eebb8b2e
Nikita Kulikov <n@kulikov.uk>
2025-03-05 13:22:32 +0300

    Fix audio GC issue

--- components/bsb/sound/impl/src/androidMain/kotlin/com/flipperdevices/bsb/sound/api/SoundFromStateProducer.kt
+++ components/bsb/sound/impl/src/androidMain/kotlin/com/flipperdevices/bsb/sound/api/SoundFromStateProducer.kt
@@ -1,9 +1,11 @@
 package com.flipperdevices.bsb.sound.api
 
-import android.content.Context
 import com.flipperdevices.bsb.preference.model.TimerSettings
+import com.flipperdevices.bsb.sound.helper.Sound
+import com.flipperdevices.bsb.sound.helper.SoundPlayHelper
 import com.flipperdevices.bsb.timer.background.model.ControlledTimerState
 import com.flipperdevices.core.di.AppGraph
+import com.flipperdevices.core.log.info
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.datetime.Clock
@@ -11,13 +13,19 @@ import me.tatarka.inject.annotations.Inject
 import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
 import kotlin.time.Duration.Companion.seconds
 
+data class SoundEventSnapshot(
+    val iteration: Int,
+    val timerSettings: TimerSettings,
+    val sound: Sound
+)
+
 @Inject
 @SingleIn(AppGraph::class)
 class SoundFromStateProducer(
     private val soundPlayHelper: SoundPlayHelper
 
 ) {
-    private var lastSoundEvent: Pair<Int, TimerSettings>? = null
+    private var lastSoundEvent: SoundEventSnapshot? = null
     private val mutex = Mutex()
 
     suspend fun tryPlaySound(state: ControlledTimerState.InProgress) = mutex.withLock {
@@ -31,12 +39,16 @@ class SoundFromStateProducer(
                 if (Clock.System.now() - state.pausedAt > 1.seconds) {
                     return@withLock
                 }
-                val soundEvent = state.currentIteration to state.timerSettings
+                val sound = when (state.type) {
+                    ControlledTimerState.InProgress.AwaitType.AFTER_WORK -> Sound.WORK_FINISHED
+                    ControlledTimerState.InProgress.AwaitType.AFTER_REST -> Sound.REST_FINISHED
+                }
+                val soundEvent = SoundEventSnapshot(
+                    state.currentIteration,
+                    state.timerSettings,
+                    sound
+                )
                 if (lastSoundEvent != soundEvent) {
-                    val sound = when (state.type) {
-                        ControlledTimerState.InProgress.AwaitType.AFTER_WORK -> Sound.WORK_FINISHED
-                        ControlledTimerState.InProgress.AwaitType.AFTER_REST -> Sound.REST_FINISHED
-                    }
                     soundPlayHelper.play(sound)
                     lastSoundEvent = soundEvent
                 }
@@ -46,16 +58,22 @@ class SoundFromStateProducer(
                 if (state.timeLeft > 4.seconds || state.timeLeft < 2.seconds) {
                     return@withLock
                 }
-                val soundEvent = state.currentIteration to state.timerSettings
-                if (lastSoundEvent != soundEvent) {
-                    val sound = when (state) {
-                        is ControlledTimerState.InProgress.Running.LongRest,
-                        is ControlledTimerState.InProgress.Running.Rest -> Sound.REST_COUNTDOWN
+                val sound = when (state) {
+                    is ControlledTimerState.InProgress.Running.LongRest,
+                    is ControlledTimerState.InProgress.Running.Rest -> Sound.REST_COUNTDOWN
 
-                        is ControlledTimerState.InProgress.Running.Work -> Sound.WORK_COUNTDOWN
-                    }
+                    is ControlledTimerState.InProgress.Running.Work -> Sound.WORK_COUNTDOWN
+                }
+                val soundEvent = SoundEventSnapshot(
+                    state.currentIteration,
+                    state.timerSettings,
+                    sound
+                )
+                if (lastSoundEvent != soundEvent) {
                     soundPlayHelper.play(sound)
                     lastSoundEvent = soundEvent
+                } else {
+                    info { "Skip, because ${state.currentIteration to state.timerSettings} already exist" }
                 }
             }
         }
