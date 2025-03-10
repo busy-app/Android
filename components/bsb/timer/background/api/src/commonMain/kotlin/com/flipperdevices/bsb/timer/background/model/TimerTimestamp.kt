28ca956bb49500904c2f053f99882e78a5c1fd9b
Roman Makeev <makeevrserg@gmail.com>
2025-03-10 14:16:14 +0300

    add lastsync item to TimerTimestamp.kt

--- components/bsb/timer/background/api/src/commonMain/kotlin/com/flipperdevices/bsb/timer/background/model/TimerTimestamp.kt
+++ components/bsb/timer/background/api/src/commonMain/kotlin/com/flipperdevices/bsb/timer/background/model/TimerTimestamp.kt
@@ -5,10 +5,19 @@ import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
 
+/**
+ * [TimerTimestamp] shared synchronization model for timer
+ * @param settings timer settings to determine new state
+ * @param start time when timer was started
+ * @param pause time when pause was clicked
+ * @param confirmNextStepClick time when next step was clicked after autopause
+ * @param lastSync time when sync of this item was received on device
+ */
 @Serializable
 data class TimerTimestamp(
     val settings: TimerSettings,
     val start: Instant = Clock.System.now(),
     val pause: Instant? = null,
-    val confirmNextStepClick: Instant = Instant.DISTANT_PAST
+    val confirmNextStepClick: Instant = Instant.DISTANT_PAST,
+    val lastSync: Instant = Clock.System.now()
 )
