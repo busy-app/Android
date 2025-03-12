fd33b09701d75798be345e07679f7b13390b703e
Nikita Kulikov <n@kulikov.uk>
2025-03-12 14:02:44 +0300

    Fix lint

--- components/core/ui/video/src/androidMain/kotlin/com/flipperdevices/ui/video/BSBVideoPlayer.kt
+++ components/core/ui/video/src/androidMain/kotlin/com/flipperdevices/ui/video/BSBVideoPlayer.kt
@@ -1,17 +1,22 @@
 package com.flipperdevices.ui.video
 
 import android.content.Context
-import android.net.Uri
 import androidx.annotation.OptIn
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.media3.common.C
-import androidx.media3.common.Player
-import androidx.compose.runtime.remember
-import androidx.compose.ui.layout.ContentScale
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
@@ -20,6 +25,7 @@ import androidx.media3.ui.compose.PlayerSurface
 import androidx.media3.ui.compose.SURFACE_TYPE_TEXTURE_VIEW
 import androidx.media3.ui.compose.modifiers.resizeWithContentScale
 import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
 
 @OptIn(UnstableApi::class)
 @Composable
@@ -31,20 +37,39 @@ actual fun BSBVideoPlayer(
 ) {
     val context = LocalContext.current
     val exoPlayer = rememberPlayer(context)
+    var isPlayerReady by remember { mutableStateOf(false) }
 
     LaunchedEffect(exoPlayer, uri) {
+        exoPlayer.addListener(object : Player.Listener {
+            override fun onRenderedFirstFrame() {
+                isPlayerReady = true
+            }
+        })
         exoPlayer.addMediaItem(MediaItem.fromUri(uri))
         exoPlayer.prepare()
     }
 
-    PlayerSurface(
-        modifier = modifier.resizeWithContentScale(
-            contentScale = ContentScale.FillWidth,
-            sourceSizeDp = null
-        ),
-        player = exoPlayer,
-        surfaceType = SURFACE_TYPE_TEXTURE_VIEW,
-    )
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        PlayerSurface(
+            modifier = Modifier.resizeWithContentScale(
+                contentScale = ContentScale.FillWidth,
+                sourceSizeDp = null
+            ),
+            player = exoPlayer,
+            surfaceType = SURFACE_TYPE_TEXTURE_VIEW,
+        )
+
+        if (!isPlayerReady) {
+            Image(
+                modifier = Modifier,
+                painter = painterResource(firstFrame),
+                contentDescription = null
+            )
+        }
+    }
 }
 
 @OptIn(UnstableApi::class)
