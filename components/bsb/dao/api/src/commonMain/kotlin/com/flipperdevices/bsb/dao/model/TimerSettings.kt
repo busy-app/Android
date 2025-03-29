4d2c60d2c1ced8938754d877bfc71169f0e11bfe
Nikita Kulikov <n@kulikov.uk>
2025-03-29 15:55:32 +0000

    Add serial name for timer settings

--- components/bsb/dao/api/src/commonMain/kotlin/com/flipperdevices/bsb/dao/model/TimerSettings.kt
+++ components/bsb/dao/api/src/commonMain/kotlin/com/flipperdevices/bsb/dao/model/TimerSettings.kt
@@ -1,5 +1,6 @@
 package com.flipperdevices.bsb.dao.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlin.jvm.JvmInline
 import kotlin.time.Duration
@@ -11,24 +12,36 @@ value class TimerSettingsId(val id: Long)
 
 @Serializable
 data class TimerSettings(
+    @SerialName("id")
     val id: TimerSettingsId,
+    @SerialName("total_time")
     val totalTime: Duration = 90.minutes,
+    @SerialName("intervals_settings")
     val intervalsSettings: IntervalsSettings = IntervalsSettings(),
+    @SerialName("sound_settings")
     val soundSettings: SoundSettings = SoundSettings(),
+    @SerialName("name")
     val name: String = "BUSY",
 ) {
     @Serializable
     data class IntervalsSettings(
+        @SerialName("work")
         val work: Duration = 25.minutes,
+        @SerialName("rest")
         val rest: Duration = 5.minutes,
+        @SerialName("long_rest")
         val longRest: Duration = 15.minutes,
+        @SerialName("auto_start_work")
         val autoStartWork: Boolean = true,
+        @SerialName("auto_start_rest")
         val autoStartRest: Boolean = true,
+        @SerialName("is_enabled")
         val isEnabled: Boolean = false
     )
 
     @Serializable
     data class SoundSettings(
+        @SerialName("alert_when_interval_ends")
         val alertWhenIntervalEnds: Boolean = true
     )
 }
