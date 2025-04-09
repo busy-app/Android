8f5a73281798e5ff15068cc251ae058bc84f6eb5
Roman Makeev <makeevrserg@gmail.com>
2025-04-09 13:26:52 +0300

    fix lag on data received with wear

--- components/bsb/wear/bridge/messenger/impl/src/androidMain/kotlin/com/flipperdevices/bsb/wear/messenger/consumer/WearDataLayerRegistryMessageConsumer.kt
+++ components/bsb/wear/bridge/messenger/impl/src/androidMain/kotlin/com/flipperdevices/bsb/wear/messenger/consumer/WearDataLayerRegistryMessageConsumer.kt
@@ -4,11 +4,14 @@ import com.flipperdevices.bsb.wear.messenger.serializer.DecodedWearMessage
 import com.flipperdevices.bsb.wear.messenger.serializer.WearMessageSerializer
 import com.flipperdevices.core.di.AppGraph
 import com.flipperdevices.core.log.LogTagProvider
+import com.flipperdevices.core.log.TaggedLogger
 import com.flipperdevices.core.log.error
 import com.flipperdevices.core.log.info
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import me.tatarka.inject.annotations.Inject
 import software.amazon.lastmile.kotlin.inject.anvil.ContributesBinding
@@ -18,7 +21,9 @@ import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
 @Inject
 @SingleIn(AppGraph::class)
 @ContributesBinding(AppGraph::class, WearMessageConsumer::class)
-class WearDataLayerRegistryMessageConsumer : WearMessageConsumer, LogTagProvider {
+class WearDataLayerRegistryMessageConsumer(
+    private val scope: CoroutineScope
+) : WearMessageConsumer, LogTagProvider {
     override val TAG: String = "WearDataLayerRegistryMessageConsumer"
     private val messageChannel = MutableSharedFlow<DecodedWearMessage<*>>()
     override val messagesFlow: Flow<DecodedWearMessage<*>> = messageChannel.asSharedFlow()
@@ -29,7 +34,9 @@ class WearDataLayerRegistryMessageConsumer : WearMessageConsumer, LogTagProvider
                 path = message.path,
                 value = message.decode(byteArray)
             )
-            runBlocking { messageChannel.emit(decodedWearMessage) }
+            scope.launch {
+                messageChannel.emit(decodedWearMessage)
+            }
         }.onFailure { throwable ->
             error(throwable) { "#consume: could not publish message: ${throwable.stackTraceToString()}" }
         }.onSuccess {
