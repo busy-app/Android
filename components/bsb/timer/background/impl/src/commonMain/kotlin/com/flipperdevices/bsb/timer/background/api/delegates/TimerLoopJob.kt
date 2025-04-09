8f13f208530a6958d739907b8e3f8f6d43571e28
Roman Makeev <makeevrserg@gmail.com>
2025-04-09 13:27:34 +0300

    fix redundant update timer state

--- components/bsb/timer/background/impl/src/commonMain/kotlin/com/flipperdevices/bsb/timer/background/api/delegates/TimerLoopJob.kt
+++ components/bsb/timer/background/impl/src/commonMain/kotlin/com/flipperdevices/bsb/timer/background/api/delegates/TimerLoopJob.kt
@@ -6,16 +6,21 @@ import com.flipperdevices.bsb.timer.background.model.TimerTimestamp
 import com.flipperdevices.bsb.timer.statefactory.TimerStateFactory
 import com.flipperdevices.core.ktx.common.withLock
 import com.flipperdevices.core.log.LogTagProvider
+import com.flipperdevices.core.log.info
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.measureTime
 
 class TimerLoopJob(
     scope: CoroutineScope,
@@ -29,11 +34,15 @@ class TimerLoopJob(
 
     internal fun getInternalState(): StateFlow<ControlledTimerState> = timerStateFlow.asStateFlow()
 
-    private val job = TickFlow()
+    private val job = TickFlow(duration = 1000.milliseconds)
         .filter { initialTimerTimestamp.runningOrNull?.pause == null }
         .onEach {
             mutex.withLock {
-                timerStateFlow.emit(timerStateFactory.create(initialTimerTimestamp))
+                timerStateFlow.update { oldState ->
+                    val newState = timerStateFactory.create(initialTimerTimestamp)
+                    if (newState == oldState) oldState
+                    else newState
+                }
             }
         }.launchIn(scope)
 
